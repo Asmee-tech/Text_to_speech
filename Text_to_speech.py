0b620190abc53698d7c9024908561c9db92c27c4
Asmee-tech <asmee.khimani@gmail.com>
2026-07-04 10:51:43 +0200

    Implement text-to-speech feature with play button
    Added functionality to convert text input to speech and play it.

--- Text_to_speech.py
+++ Text_to_speech.py
@@ -2,6 +2,7 @@ from tkinter import*
 # from tkinter.ttk import*
 from gtts import gTTS
 import os
+# import pygame
 
 screen=Tk()
 screen.title("Text to speech")
@@ -13,12 +14,20 @@ frame1.pack(fill=X)
 frame2=Frame(screen,bg="lightgreen",height="750")
 frame2.pack(fill=X)
 
-lab1=Label(frame1,text="Text to speech",font="bold,80").place(x=80,y=100)
+lab1=Label(frame1,text="Text to speech",font="bold,80")
+lab1.place(x=250,y=100)
 
-ent=Entry(frame2,font=40,width=45).place(x=80,y=150)
+ent=Entry(frame2,font=14,width=45)
+ent.place(x=100,y=50)
 ent.insert(0,"")
 
-sub=Button(frame2,text="SUBMIT",width=15).place(x=250,y=130)
+def play():
+    language="en"
+    myobj=gTTS(text=ent.get(),lang=language,slow=False)
+    myobj.save("convert.mp3")
+    os.system("xdg-open convert.mp3")
 
+
+sub=Button(frame2,text="SUBMIT",width=15,pady=15,command=play).place(x=250,y=130)
 
 screen.mainloop()
